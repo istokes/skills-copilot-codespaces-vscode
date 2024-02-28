a11a2fcf2ad6710020c295f448a49c4d751f4001
istokes <ian.stokes@intel.com>
2024-02-28 15:57:36 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,9 @@
+function skillsMember() {
+    var skills = ["HTML", "CSS", "JavaScript"];
+    var member = {
+        name: "John",
+        age: 30,
+        skills: skills
+    };
+    return member;
+}
