f38c4e3d430448366b3e464dabdbdffb334e5270
istokes <ian.stokes@intel.com>
2024-02-28 16:18:37 +0000

    Copilot third commit

--- comments.js
+++ comments.js
@@ -1,2 +1,12 @@
-// create web server
+// Create web server
+// Load the core module http
 var http = require('http');
+// Create a web server
+var server = http.createServer(function(req, res){
+  // Set the response header with the content type
+  res.writeHead(200, {'Content-Type': 'text/plain'});
+  // Send the response body "Hello World"
+  res.end('Hello World\n');
+});
+// Listen on port 3000
+server.listen(3000, '
