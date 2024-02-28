e107963e6e8fe6d3eac4d3843f4a77d83e2608c4
istokes <ian.stokes@intel.com>
2024-02-28 16:12:55 +0000

    Copilot third commit

--- comments.js
+++ comments.js
@@ -1,13 +1,2 @@
 // create web server
 var http = require('http');
-var fs = require('fs');
-
-// create server
-http.createServer(function(req, res){
-    // read file
-    fs.readFile('index.html', function(err, data){
-        res.writeHead(200, {'Content-Type': 'text/html'});
-        res.write(data);
-        return res.end();
-    });
-}).listen(8080); // listen on port 8080
